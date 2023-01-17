ba3deb4092127135c91af474377e5053275a85d7
unknown <c00260297@itcarlow.ie>
2023-01-17 15:40:03 +0000

    NavMesh Start

---
+++ Lab 8/Assets/Scripts/Student/STUDENT_STATES.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public enum STUDENT_STATES
+{
+    GO_CLASS,
+    STAY_CLASS,
+    RUN
+}
---
+++ Lab 8/Assets/Scripts/Student/StudentAI.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class StudentAI:MonoBehaviour
+{
+
+    private STUDENT_STATES STATE;
+
+
+}
