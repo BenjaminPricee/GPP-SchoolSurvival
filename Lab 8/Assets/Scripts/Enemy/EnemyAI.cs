3a2f9bb7943852e0cf82f3e11c327479926dc3c9
unknown <c00260297@itcarlow.ie>
2023-01-20 12:49:56 +0000

    Update

--- Lab 8/Assets/Scripts/Enemy/EnemyAI.cs
+++ Lab 8/Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
     private ENEMY_STATES STATE;
+
+    public NavMeshAgent navigation;
+
+    //Transform[] Patr
+
+    private void Start()
+    {
+        navigation.updateRotation = false;
+        navigation.updateUpAxis = false;
+
+        STATE = ENEMY_STATES.PATROL;
+    }
+
+    private void Update()
+    {
+        if(STATE == ENEMY_STATES.PATROL)
+        {
+
+        }
+        else if (STATE == ENEMY_STATES.ATTACK)
+        {
+
+        }
+    }
 }
